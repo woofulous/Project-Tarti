95c557110782c76b582d2c9ca384c5add14fcf4a
woofulous <119539138+woofulous@users.noreply.github.com>
2024-03-06 19:32:49 -0800

    remove fusion

--- src/ReplicatedStorage/Controllers/DialogController.lua
+++ src/ReplicatedStorage/Controllers/DialogController.lua
@@ -2,12 +2,12 @@
 	This listens for events from the NPCService to start talking to an NPC and prepares then fires events back to the server through ModuleScripts located in the DialogTree
 ]]
 
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TweenService = game:GetService("TweenService")
 
-local Knit = require(ReplicatedStorage.Packages.Knit)
-local Fusion = require(ReplicatedStorage.Packages.Fusion)
+local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
 
 local choicePrefab = script.DialogChoiceButton
+local slideInfo = TweenInfo.new()
 
 local currentNPC = nil
 local root = nil -- set to Interface.root
@@ -34,7 +34,12 @@ function DialogController:OpenScreenAsync()
 	print("opening screen while currentNPC is:", currentNPC)
 end
 
-function DialogController:CloseDialogScreen() end
+function DialogController:CloseDialogScreen()
+	Fusion.Spring(Fusion.Value(0), )
+	task.wait(5)
+	-- apply an impulse
+	smoothPosition:addVelocity(UDim2.fromOffset(-10, 10))
+end
 
 function DialogController:KnitStart()
 	local Interface = Knit.GetController("Interface")
